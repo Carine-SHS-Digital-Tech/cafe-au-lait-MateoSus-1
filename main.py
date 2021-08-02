c096109286d194112fafd20f5214a87c09e20b07
MateoSus <mateofreychet@outlook.com>
2021-08-02 18:02:17 +0800

    Commit #2

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ Quantity_List = []
 GST = Price_List*int(1.1)
 Surcharge = GST*int(1.05)
 
-Order_Method = input("'N' for New Order,\n'T' for Take Away;\nOrder Method: ")
-if Order_Method == "N" or "n":
-    price = Surcharge
-    print("New Order")
-elif Order_Method == "T" or "t":
-    price = GST
-    print("Take-Away")
+D = "Dine-In"
+d = "Dine-In"
+T = "Take-Away"
+t = "Take-Away"
 
+Order_Method = input("[D] for Dine-In\n[T] for Take-Away\nOrder Method: ")
+if Order_Method == "D":
+    price = Surcharge
+    print(D)
+elif Order_Method == "d":
+    price = Surcharge
+    print(d)
+elif Order_Method == "T":
+    price = GST
+    print(T)
+elif Order_Method == "t":
+    price = GST
+    print(t)
+else:
+    print("Unknown command, please try again")
