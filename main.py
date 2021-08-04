4ed0b2bf31ca4f8f2835da900f86fe2e66fbde54
MateoSus <mateofreychet@outlook.com>
2021-08-04 12:32:34 +0800

    Main without operating loop

--- main.py
+++ main.py
@@ -26,16 +26,18 @@ else:
         price = GST
         print(T)
 
-item = input("\nWhat would you like to order from our menu today? (1-4):"
-             "\n•(1) Cappuccino    [$3.00]\n•(2) Espresso      [$2.25]"
-             "\n•(3) Latte         [$2.50]\n•(4) Iced Coffee   [$2.50]\n")
+
 
 count = 0
 while count < 4:
+    item = input("\nWhat would you like to order from our menu today? (1-4):"
+             "\n•(1) Cappuccino    [$3.00]\n•(2) Espresso      [$2.25]"
+             "\n•(3) Latte         [$2.50]\n•(4) Iced Coffee   [$2.50]\n")
     print(f"You have selected a {menu[int(item)-1]}")
     count = count + 1
     Quantity = input(f"How many {menu[int(item)-1]}'s would you like? ")
     print(f"{menu[int(item)-1]} * {Quantity}")
+   # if count < 4
     Order_Again = input("\nWould you like anything else? (Y/N): ")
     while Order_Again.upper() not in ["Y","N"]:
         print("Unknown command, please try again\n")
