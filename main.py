1f146c2c2de0e91ce9b9cca5bb53f5d944e27b6d
MateoSus <mateofreychet@outlook.com>
2021-08-05 22:31:09 +0800

    Yes/No loop fixed (sort of)

--- main.py
+++ main.py
@@ -15,33 +15,43 @@ T = "Take-Away"
 t = "Take-Away"
 
 Order_Method = input("[D] for Dine-In\n[T] for Take-Away\nOrder Method: ")
+Order_Type.append(Order_Method)
 while Order_Method.upper() not in ["D", "T"]:
     print("Unknown command, please try again\n")
     Order_Method = input("[D] for Dine-In\n[T] for Take-Away\nOrder Method: ")
 else:
     if Order_Method.upper() == "D":
         price = Surcharge
-        print(D)
+        print(Order_Type)
     else:
         price = GST
-        print(T)
+        print(Order_Type)
 
 again = 0
+choice = True
 count = 0
 while count < 4:
-    while again == 0:
-        item = input("\nWhat would you like to order from our menu today? (1-4):"
-                     "\n•(1) Cappuccino    [$3.00]\n•(2) Espresso      [$2.25]"
-                     "\n•(3) Latte         [$2.50]\n•(4) Iced Coffee   [$2.50]\n")
-        count = count + 1
-        print(f"You have selected a {menu[int(item) - 1]}")
-        Quantity = input(f"How many {menu[int(item) - 1]}'s would you like? ")
-        print(f"{menu[int(item) - 1]} * {Quantity}")
-        Order_Again = input("\nWould you like anything else? (Y/N): ")
-        while Order_Again.upper() not in ["Y", "N"]:
-            print("Unknown command, please try again")
-        if Order_Again.upper() == "N":
-            again = 1
-        if Order_Again.upper() == "Y":
-            item = input("What else would you like from our menu? (1-4): ")
+    while choice == True:
+        while again == 0:
+            item = input("\nWhat would you like to order from our menu today? (1-4):"
+                         "\n•(1) Cappuccino    [$3.00]\n•(2) Espresso      [$2.25]"
+                         "\n•(3) Latte         [$2.50]\n•(4) Iced Coffee   [$2.50]\n")
+            count = count + 1
+            print(f"You have selected a {menu[int(item) - 1]}")
+            Quantity = input(f"How many {menu[int(item) - 1]}'s would you like? ")
+            print(f"{menu[int(item) - 1]} * {Quantity}")
+            while count < 4:
+                Order_Again = input("\nWould you like anything else? (Y/N): ")
+                while Order_Again.upper() not in ["Y", "N"]:
+                    print("Unknown command, please try again")
+                if Order_Again.upper() == "Y":
+                    item = input("\nWhat else would you like from our menu? (1-4): ")
+                    count = count + 1
+                    print(f"You have selected a {menu[int(item) - 1]}")
+                    Quantity = input(f"How many {menu[int(item) - 1]}'s would you like? ")
+                    print(f"{menu[int(item) - 1]} * {Quantity}")
+                if Order_Again.upper() == "N":
+                    choice = False
+            if count == 4:
+                again = 1
 
