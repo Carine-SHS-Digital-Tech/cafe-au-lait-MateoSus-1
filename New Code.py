aabfa331f8d175e7330888ca02fbd63011f3363b
MateoSus <mateofreychet@outlook.com>
2021-08-08 18:03:04 +0800

    Receipt starting to work

--- New Code.py
+++ New Code.py
@@ -12,9 +12,9 @@ Item_List = []
 Quantity_List = []
 Price_List = []
 
-Operation = 0
+Operation = " "
 a = 0
-item = 0
+Item = 0
 
 TakeAway_Option = " "
 DineIn_Option = " "
@@ -23,6 +23,10 @@ Cappuccino_Quantity = 0
 Espresso_Quantity = 0
 Latte_Quantity = 0
 IcedCoffee_Quantity = 0
+
+TotalExcluding_GST = float(0)
+TotalIncluding_GST = float(0)
+Total = float(0)
 
 Cappuccino_Price = float(3.00)
 Espresso_Price = float(2.25)
@@ -36,12 +40,10 @@ GST = Price_List * int(1.1)
 Surcharge = GST * int(1.05)
 
 while Operation != "3":
-    Operation = input("""
-•(1) New order\n•(2) Daily Summary\n•(3) Exit\nOperation number (1-3): """)
+    Operation = input("•(1) New order\n•(2) Daily Summary\n•(3) Exit\nOperation number (1-3): ")
     if Operation == "1":
         while a == 0:
-            a = input("""
-Order Type:\n•(1) Dine-In\n•(2) Take-Away\n""")
+            a = input("\nOrder Type:\n•(1) Dine-In\n•(2) Take-Away\n")
             if a == "1":
                 DineIn_Number = DineIn_Number + 1
                 DineIn_Option = " "
@@ -51,43 +53,59 @@ Order Type:\n•(1) Dine-In\n•(2) Take-Away\n""")
             else:
                 print("Unknown command, please try again.")
                 a = 0
-        while item != " ":
-            item = input("""
-Menu :\n•(1)Cappuccino    [$3.00]\n•(2)Espresso      [$2.25]\n•(3)Latte         [$2.50]\n•(4)Iced Coffee   [$2.50]\n•Press 'Enter' to print reciept""")
-            if item == "1":
-                Cappuccino_Quantity = input("Cappuccino quantity: ")
-                print(f"Cappuccino * {Cappuccino_Quantity}")
+        while Item != "":
+            Item = input("Menu :\n•(1)Cappuccino    [$3.00]\n•(2)Espresso      [$2.25]\n•(3)Latte         [$2.50]\n•("
+                         "4)Iced Coffee   [$2.50]\n•'ENTER' to print reciept\nItem choice: ")
+            if Item == "1":
+                Cappuccino_Quantity = int(input("Cappuccino quantity: "))
+                print(f"Cappuccino * {Cappuccino_Quantity}\n")
                 Cappuccino_Total = Cappuccino_Quantity * Cappuccino_Price
                 Quantity_List.append(Cappuccino_Quantity)
                 Price_List.append(Cappuccino_Total)
-                Item_List.append("Cappuccino")
-
-            if item == "2":
-                Espresso_Quantity = input("Espresso quantity: ")
-                print(f"Espresso * {Espresso_Quantity}")
+                Item_List.append("Cappuccino   ")
+                Item = " "
+            elif Item == "2":
+                Espresso_Quantity = int(input("Espresso quantity: "))
+                print(f"Espresso * {Espresso_Quantity}\n")
                 Espresso_Total = Espresso_Quantity * Espresso_Price
                 Quantity_List.append(Espresso_Quantity)
                 Price_List.append(Espresso_Total)
-                Item_List.append("Espresso")
-
-            if item == "3":
-                Latte_Quantity = input("Latte quantity: ")
-                print(f"Latte * {Latte_Quantity}")
+                Item_List.append("Espresso     ")
+                Item = " "
+            elif Item == "3":
+                Latte_Quantity = int(input("Latte quantity: "))
+                print(f"Latte * {Latte_Quantity}\n")
                 Latte_Total = Latte_Quantity * Latte_Price
                 Quantity_List.append(Latte_Quantity)
                 Price_List.append(Latte_Total)
-                Item_List.append("Latte")
-
-            if item == "4":
-                IcedCoffee_Quantity = input("Iced Coffee quantity: ")
-                print(f"Iced Coffee * {IcedCoffee_Quantity}")
+                Item_List.append("Latte        ")
+                Item = " "
+            elif Item == "4":
+                IcedCoffee_Quantity = int(input("Iced Coffee quantity: "))
+                print(f"Iced Coffee * {IcedCoffee_Quantity}\n")
                 IcedCoffee_Total = IcedCoffee_Quantity * IcedCoffee_Price
                 Quantity_List.append(IcedCoffee_Quantity)
                 Price_List.append(IcedCoffee_Total)
-                Item_List.append("Iced Coffee")
-
-            elif item != '':
+                Item_List.append("Iced Coffee  ")
+                Item = " "
+            elif Item != "":
                 print("Unknown command, please try again.")
+
+            else:
+                print("            CAFE AU LAIT             ")
+                print("* * * * * * * * * * * * * * * * * * *")
+                print("               RECEIPT               ")
+                print("* * * * * * * * * * * * * * * * * * *")
+                print("DESCRIPTION                     PRICE")
+                b = 0
+                for c in range(len(Item_List)):
+                    print(f"{Item_List[b]} * {str(Quantity_List[b])}               ${str(round(Price_List[b], 2))}0")
+                    Total = Total + Price_List[b]
+                    b = b + 1
 
     elif Operation == "2":
         os.system("start EXCEL.EXE daily_summary.csv")
+    elif Operation == "3":
+        break
+    else:
+        print("Unknown command, please try again.")
